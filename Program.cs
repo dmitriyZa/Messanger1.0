0002d67c36e68a12c07830778af8f70095d6ee63
Дмитрий Сергеевич Загарий <d.zagariy@hq.int-sys.ru>
2021-08-27 23:17:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Messanger1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message("Rusal", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Message desmsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(output);
+            //Console.WriteLine(msg.ToString());
+        }
+    }
+}
